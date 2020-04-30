1eb8fa285530041569426568e20e9cd4b2fc1459
lampardN <46028845+lampardN@users.noreply.github.com>
2020-04-30 13:19:39 +0500

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-a = [int(input()) for i in range(n)]
-count = 0
-for i in range(n-5):
-    for j in range(i+5, n):
-        if (a[i] + a[j]) % 2 == 1 and (a[i]*a[j]) % 13 == 0:
-            count += 1
-print(count)
